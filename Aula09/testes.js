cb72ea40e051d023da8ac72d24e943ce86f65012
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2022-10-04 20:33:48 -0300

    aula09
    aula09

---
+++ Aula09/testes.js
@@ -0,0 +1,14 @@
+function media(a,b,c){
+    //return (a+b+c) / 3
+    console.log('A média é ' + ((a+b+c) / 3))
+}
+
+var x = 10
+var y = 9
+var z = 8
+//media = (x+y+z) / 3
+//console.log(`A média é ${media}`)
+//console.log('A média é ' + media)
+//console.log('A média é ' + media(x,y,z))
+media(x,y,z)
+media(11,12,14)
