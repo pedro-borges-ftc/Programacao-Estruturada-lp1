000cf380735c1573bc302c2aec7202b54880e9ee
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2022-11-27 18:26:34 -0300

    fim semestre 2022.2

--- Aula13/testes.js
+++ Aula13/testes.js
@@ -8,12 +8,12 @@ console.log(a.pop())    //removendo o elemento no fim do array
 console.log(a.length)
 
 //Iterando um array
-var cores = ['vermelho', 'verde', 'azul'];
-
-for (var i = 0; i < cores.length; i++) {
-  console.log(`Índice: ${i} - Valor: ${cores[i]}`);
-}
+var cores = ['vermelho', 'verde', 'azul', 'branco'];
 
 for(i in cores){
     console.log(`Índice: ${i} - Valor: ${cores[i]}`);
+}
+
+for (var i = 0; i < cores.length; i++) {
+  console.log(`Índice: ${i} - Valor: ${cores[i]}`);
 }
